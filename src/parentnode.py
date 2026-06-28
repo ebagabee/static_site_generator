1c7bf6ed8001baabd886cf5882f48714db76ef36
ebagabee <alvesantos.gh@gmail.com>
2026-06-28 20:04:02 -0300

    feat: create a new to_html method

--- src/parentnode.py
+++ src/parentnode.py
@@ -4,3 +4,17 @@ class ParentNode(HTMLNode):
     def __init__(self, tag, children, props = None):
         super().__init__(tag=tag, value=None, children=children, props=props)
     
+    def to_html(self):
+        if self.tag is None:
+            raise ValueError()
+        
+        if self.children is None:
+            raise ValueError("Children is missing")
+        
+        result = ""
+        
+        for children in self.children:
+            result += children.to_html()
+
+        return f"<{self.tag}{self.props_to_html()}>{result}</{self.tag}>"
+        
